77540fca53f35dd3ced174199c4ab3794848e372
Walid Ashraf <walidashraf050@gmail.com>
2025-08-05 22:34:41 +0300

    Connect to a MongoDB Database

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.sendFile("./views/home.html", {root: __dirname})
-})
+app.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: __dirname });
+});
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+mongoose
+  .connect(
+    "mongodb+srv://walidashraf050:UCNCUQIhZmJygbWI@cluster0.chxe8xl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
